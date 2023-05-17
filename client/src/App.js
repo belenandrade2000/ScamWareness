c877f79ad0c1046bc261a14246e9d302ab43117c
Belen Andrade <belenandrade2000@gmail.com>
2023-05-17 14:42:11 -0700

    started profile

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,6 @@ import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-
-
 import SavedCards from './pages/SavedCards';
 import Signup from './pages/SignUp';
 import Login from './pages/Login';
