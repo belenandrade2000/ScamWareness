e62ba82bf763deb5eb85f2a0efdd874be338410f
Belen Andrade <belenandrade2000@gmail.com>
2023-05-17 11:51:58 -0700

    got login and signup

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,10 +4,6 @@ export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(name: $username, email: $email, password: $password) {
       token
-      user {
-        _id
-        name
-      }
     }
   }
 `;
@@ -16,10 +12,6 @@ export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      profile {
-        _id
-        name
-      }
     }
   }
 `;
