b3cbe5bf623b2f23eead1cecd397da4c1066217f
em1nm <em.martirosyan@gmail.com>
2023-05-19 12:17:58 -0700

    fixed CCHome.js file

--- client/src/pages/ccHome.js
+++ client/src/pages/ccHome.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// its a string before you converrt it into function
+//string before you convert it into function
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_CC } from "../utils/queries";
 import {SAVE_CARD} from "../utils/mutations"
